61711bd6605ff8bf0b3b088f82d3efeee83d53d5
Jerry Diaz <luis.diaz@cceo.com.mx>
2019-07-03 11:22:41 -0500

    Creation of the client model and the ClientController

---
+++ app/Client.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = ['namec','type_doc','doc_num','address','phone_num','email'];
+}
