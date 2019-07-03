8ff8215266398ed7b69b3a00b29713bf738e1d20
Jerry Diaz <luis.diaz@cceo.com.mx>
2019-07-03 11:16:55 -0500

    Migration of the clients table

---
+++ database/migrations/2019_07_03_160302_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('namec', 100)->unique();
+            $table->string('type_doc', 25)->nullable();
+            $table->string('doc_num', 25)->nullable();
+            $table->string('address', 70)->nullable();
+            $table->string('phone_num', 20)->nullable();
+            $table->string('email', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
