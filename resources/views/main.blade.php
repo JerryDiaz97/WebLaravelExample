f01a75a53c95a808778be4698e4d5b5d77ef217d
Jerry Diaz <luis.diaz@cceo.com.mx>
2019-07-05 14:18:58 -0500

    Problem to logout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -66,7 +66,7 @@
                     </div>
                     <a class="dropdown-item" href="{{ route('logout') }}" 
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    <i class="fa fa-lock"></i>Cerrar sesión</a>
+                    <i class="fa fa-lock"></i> Cerrar sesión</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }}
                     </form>
